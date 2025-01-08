b07855f384265488fd4000877a8580d6105c1ed0
AlCh440 <73243813+AlCh440@users.noreply.github.com>
2025-01-08 18:46:24 +0100

    improved ui

---
+++ 2d Platformer/Assets/Scripts/PanelOrder.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOrder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pos = 0.0f;
+        foreach (Transform child in transform)
+        {
+            Debug.Log(child.gameObject.name);
+            child.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(120.0f, 200.0f + pos);
+            pos -= 100.0f;
+        }
+        //foreach (GameObject obj in gameObject)
+        //{
+        //    obj.transform.position = transform.position + new Vector2(3.0f, 3.0f, 0, 0f);
+        //}
+    }
+}
